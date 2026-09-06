c7a1c90f9983cf3e3feb417db9b1423e82b570fa
unknown <zambracele@gmail.com>
2026-09-05 23:24:34 -0300

    Modificacion en FichaTurno

--- src/components/ConsultaTurno/ConsultaTurno.jsx
+++ src/components/ConsultaTurno/ConsultaTurno.jsx
@@ -1,17 +1,8 @@
-import { useState } from 'react'
-
-import Navbar from './components/navbar/Navbar'
-import Footer from './components/footer/Footer'
-import Main from './components/main/Main'
 import FichaDeTurno from "../FichaDeTurno/FichaDeTurno";
 
-function App() {
-  // const [count, setCount] = useState(0)
-
+function ConsultaTurnos() {
   return (
-    <div className="d-flex flex-column min-vh-100">
-      <Navbar />
-      <Main/>
+    <div>
       <FichaDeTurno cargando={true} />
       <FichaDeTurno error={true} />
       <FichaDeTurno turno={null} />
@@ -29,9 +20,8 @@ function App() {
         observaciones: "",
         sesiones: 0
       }} />
-      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default ConsultaTurnos;
